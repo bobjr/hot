a25380e04110313f7cfceb675b5a580ef75d0af6
funky gao <funky.gao@gmail.com>
2012-12-12 21:17:37 +0800

    c coding

---
+++ c/erf.c
@@ -0,0 +1,8 @@
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+    printf("The integral of a Normal(0, 1) distribution "
+            "between -1.96 and 1.96 is %g\n", erf(1.96*sqrt(1/2.)));
+}
+            
